f0b5a7092d3088c2896e5d45a5810e94616239f1
j0org <j0org@German>
2023-12-12 19:02:11 -0600

    Primer commit

---
+++ src/config/Conexion.java
@@ -0,0 +1,37 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    Connection con;
+    static String url = "jdbc:mysql://localhost:3306/punto_de_venta?useSSL=false";
+    static String user = "root";
+    static String pass = "";
+
+    public Connection Conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de Conexion a Base de Datos : " + e);
+        }
+        return con;
+
+    }
+
+    public static Connection ConectarDB() {
+        Connection conex = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conex = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de Conexion a Base de Datos : " + e);
+        }
+        return conex;
+
+    }
+}
